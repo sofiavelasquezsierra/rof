1b8c5a9581ce8f51b3c16e69414e6b99be810db8
Mustafa Kazmi <Mustafa@Saiyids-MacBook-Pro.local>
2024-12-06 21:37:50 -0500

    NAV bar and color pallete in tailwin.config.ts file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import theme, { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.tsx"],
@@ -10,5 +10,18 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+    daisyui: {
+      themes: [
+        {
+          mytheme: {
+            primary: "#6f49ceff",
+            secondary: "#aeb8feff",
+            accent: "#f1f2f6ff",
+            neutral: "#f1555dff",
+            coral: "f68e93ff",
+          },
+        }
+      ]
+  }
 } satisfies Config;
