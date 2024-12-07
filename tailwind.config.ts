30b2729f1b6bb5e357903ef40ed35cd17f6f402f
Mustafa Kazmi <Mustafa@Saiyids-MacBook-Pro.local>
2024-12-06 21:54:15 -0500

    Font and import google fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,22 +6,32 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        title: ['Poppins', 'Arial', 'Helvetica', 'sans-serif'], // Headings
+        body: ['Roboto', 'Arial', 'Helvetica', 'sans-serif'],  // Body text
+        error: ['Roboto', 'Arial', 'Helvetica', 'sans-serif'], // Error messages
+      },
+      fontWeight: {
+        semibold: 600, // Semi-bold
+        normal: 400,   // Normal
+        bold: 700,     // Bold
+      },
+      colors: {
+        error: '#FF0000', // Red for error messages
       },
     },
   },
   plugins: [require('daisyui')],
-    daisyui: {
-      themes: [
-        {
-          mytheme: {
-            primary: "#6f49ceff",
-            secondary: "#aeb8feff",
-            accent: "#f1f2f6ff",
-            neutral: "#f1555dff",
-            coral: "f68e93ff",
-          },
-        }
-      ]
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#6f49ceff", //Purple
+          secondary: "#aeb8feff", //Pink
+          accent: "#f1f2f6ff", //White
+          neutral: "#f1555dff", //Red
+          coral: "f68e93ff", //Coral
+        },
+      }
+    ]
   }
-} satisfies Config;
+};
