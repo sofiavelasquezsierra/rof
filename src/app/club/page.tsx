645e6c185dda84c7f610ece875af3c82d698913c
Mustafa Kazmi <Mustafa@Saiyids-MacBook-Pro.local>
2024-12-11 01:11:03 -0500

    minor

--- src/app/club/page.tsx
+++ src/app/club/page.tsx
@@ -1,27 +1,33 @@
-import React from 'react'
+import React from "react";
+import { db } from "@/server/db";
+import { students } from "~/server/db/schema";
 
 interface User {
-    id: number;
-    name: string;
-    username: string;
-    email: string;
+  id: number;
+  fname: string;
+  lname: string;
+  email: string;
 }
 
-const UsersPage = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users',
-    { next: { revalidate: 60 } }); // revalidate every 60 seconds
-    const users: User[] = await res.json();
-    
+export async function UsersPage() {
+  const students = await db.query.students.findMany();
+  console.log(students);
+
   return (
     <div>
+      <main>
+        {students.map((student) => (
+          <ul key={student.id}>
+            <li>
+              {student.fname}, {student.email}, {student.lname}
+            </li>
+          </ul>
+        ))}
         <h1>Users</h1>
         <p>{new Date().toLocaleTimeString()}</p>
-        <ul>
-            {users.map(user => <li key={user.id}>{user.name}, {user.email}</li>)}
-        </ul>
-        
+      </main>
     </div>
-  )
+  );
 }
 
-export default UsersPage
+export default UsersPage;
