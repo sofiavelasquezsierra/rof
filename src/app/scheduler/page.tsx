bb2885b20ad891d3baf5e5a54f294b91dca940b2
Mustafa Kazmi <Mustafa@Saiyids-MacBook-Pro.local>
2024-12-17 01:25:55 -0500

    Calendar

--- src/app/scheduler/page.tsx
+++ src/app/scheduler/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Scheduler from "../_components/Scheduler";
+import SideBarClub from "../_components/SideBarClub";
 
 export default function CreateEventPage() {
   return (
     <div>
+      <SideBarClub />
       <Scheduler />
     </div>
   );
